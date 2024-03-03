e9a7f6ef632f6daff2062da910ff7ce647fc7e76
Mariusz <pinkiweb88@gmail.com>
2024-03-03 20:08:05 +0100

    generating background

--- tutorial.py
+++ tutorial.py
@@ -15,9 +15,23 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background",name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+
+
 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
