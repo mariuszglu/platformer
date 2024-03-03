2873205fc4b50518970fb16fdd6451eac8363861
Mariusz <pinkiweb88@gmail.com>
2024-03-03 20:40:48 +0100

    declaration class Player and inheret from class Sprite (special class to handle using in collision)

--- tutorial.py
+++ tutorial.py
@@ -15,6 +15,24 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+
+
+class Player(pygame.sprite.Sprite):
+    #Sprite is accurate in the event of collisions
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0 #velocity
+        self.y_vel = 0
+        self.mask = None
+    
+
+
+
+
+
+
 def get_background(name):
     image = pygame.image.load(join("assets", "Background",name))
     _, _, width, height = image.get_rect()
