7e8c290d7aa41dd4ce28f95bf5aa49b2435bc513
Mariusz <pinkiweb88@gmail.com>
2024-03-03 19:05:13 +0100

    define a few global variables

--- tutorial.py
+++ tutorial.py
@@ -6,3 +6,9 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 
+pygame.display.set_caption("Platformer")
+
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
