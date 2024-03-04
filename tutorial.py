2c34fcab220422e4d5f0ddfa5bc40cd2d0f1778b
Mariusz <pinkiweb88@gmail.com>
2024-03-04 21:24:51 +0100

    loading/spliting sprite sheets

--- tutorial.py
+++ tutorial.py
@@ -15,13 +15,36 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+            
 
 
 class Player(pygame.sprite.Sprite):
     #Sprite is accurate in the event of collisions
     COLOR = (255, 0, 0)
     GRAVITY = 1 #begining acceleration gravity
-
+    
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0 #velocity
