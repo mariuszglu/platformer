2c0fb86b254e615e7a43a2ae667177e7bc43fe6a
Mariusz <pinkiweb88@gmail.com>
2024-03-05 05:54:56 +0100

    animating the player, creating update_sprite metod

--- tutorial.py
+++ tutorial.py
@@ -50,6 +50,7 @@ class Player(pygame.sprite.Sprite):
 
     #grab all images
     SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DELEY = 3
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -81,12 +82,24 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count +=1
+        self.update_sprite()
 
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+        
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELEY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
 
     def draw(self, win):
         # pygame.draw.rect(win, self.COLOR, self.rect)
         # self.sprite = self.SPRITES["idle"][0]
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        # self.sprite = self.SPRITES["idle_" + self.direction][0] __ remove
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
