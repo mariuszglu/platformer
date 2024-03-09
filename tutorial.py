5bb30c90f57d9d6a784ff2ceb5545958675200f0
Mariusz <pinkiweb88@gmail.com>
2024-03-09 08:49:35 +0100

    add handle_vertical_collision method

--- tutorial.py
+++ tutorial.py
@@ -156,9 +156,23 @@ def draw(window, background, bg_image, player, objects):
     player.draw(window)
 
     pygame.display.update()
+    
+
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+        collided_objects.append(obj)
+    return collided_objects
 
 
-def handle_move(player):
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
